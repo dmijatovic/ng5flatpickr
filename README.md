bd125057f1e8dc4729527c16381ce96bf40a4770
Dusan Mijatovic (Zenbook) <d.mijatovic@dv4all.nl>
2017-12-09 16:02:37 +0100

    locale and time only examples

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ng5flatpickr demo
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.4.
-It is used to test implementation of flatpickr library in an Angular 5 project. On the flatpicker repo website you will find excellent documentation and lot of examples. This project is simply used to practice these examples and combine them in the way I need these in mine projects. Using separate project to test ensures that no other 'third party libraries' interferre and influence the outcome of testing. 
+It is used to test implementation of flatpickr library in an Angular 5 project. On the flatpickr website you can find an excellent documentation and lot of examples. This project is used to practice these examples and combine them in the way I need them in mine projects. Using separate project for testing ensures that no other 'third party libraries' scripts interferre / influence the outcome of test. If during integration into a project the outcome/output differs from this test we can be certain that some other library influences performance of flatpickr.
 
 ## Development server
 
@@ -27,7 +27,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
 
-## [Datepickr](https://chmln.github.io/flatpickr/)
+## [Flatpickr](https://chmln.github.io/flatpickr/)
 
 flatpickr is a lightweight and powerful datetime picker. Lean, UX-driven, and extensible, yet it doesn’t depend on any libraries. There’s minimal UI but many themes. Rich, exposed APIs and event system make it suitable for any environment. More info is available at [flatpickr repo](https://chmln.github.io/flatpickr/).
+
+For integration follow the steps in documentation except for CSS integration. In our case we will pull styles from local node_module dir into out main.scss file. 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+//import flatpicker lib
 import * as fp from 'flatpickr';
+//import locales 
+import { Dutch } from "flatpickr/dist/l10n/nl.js";
+import { Russian } from "flatpickr/dist/l10n/ru.js";
+
 
 @Component({
   selector: 'app-root',
@@ -9,76 +14,168 @@ import * as fp from 'flatpickr';
 })
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'flatpicker demo';
-  sd1:any;
-  ed1:any;
-  sd2:any;
-  ed2:any;
-
+  sd:any=[];
+  ed:any=[];
+  st:any=[];
+  et:any=[];
+  //sd2:any;
+  //ed2:any;
   ngOnInit(){
     console.log("On init");
   }
 
   ngAfterViewInit(){
     console.log("After view init");
-    
-    this.sd1 = fp("#startDate1",{
+    //load datepickers
+    this.initSample1();
+    this.initSample2();
+    this.initSample3();
+  }  
+//--------------------------------------------------------------
+//SAMPLE 1 - DATE AND TIME, MINDATE and TIME based on user input
+  initSample1(){
+    this.initSd0();
+    this.initEd0();
+  }
+
+  initSd0(){
+    this.sd[0] = fp("#sd0",{
       dateFormat:'d-m-Y at H:i',
       enableTime: true,
       time_24hr:true,
-      onOpen: this.sd1Open,
-      onChange: this.sd1Changed,
-      onClose: this.sd1Closed,
+      onOpen: this.sd0Open.bind(this),
+      onChange: this.sd0Changed.bind(this),
+      onClose: this.sd0Closed.bind(this),
       minuteIncrement: 5
     });
+  }
 
-    
-    this.ed1 = fp("#endDate1",{
+  sd0Open(x,y,z){
+    console.log("sd0 open...",x,y,z);
+  }
+
+  sd0Changed(selDates,strDate,instance){
+    console.log("sd0 changed...", strDate);
+  }  
+  /**
+   * Flatpickr onClose event function
+   * NOTE! when passing this function to flatpicker bind this to this class
+   * in order to call class functions when flatpicker closed event occure
+   * @param selDates array of dates
+   * @param strDate string formated date (show in input)
+   * @param instance flatpickr instance
+   */
+  sd0Closed(selDates,strDate,instance){
+    console.log("sd0 closed...", strDate);
+    //debugger
+    //set minDate value of ed0
+    this.ed0MinDate(selDates[0]);
+  }
+  initEd0(){
+    this.ed[0] = fp("#ed0",{
       dateFormat:'d-m-Y at H:i',
       enableTime: true,
       time_24hr:true,
-      onOpen:this.ed1Open
+      onOpen:this.ed0Open.bind(this)
     });
-
-    this.sd2 = fp("#startDate2",{
-      dateFormat:'d-m-Y',
-      //enableTime: true,
-      //time_24hr:true
-    });
-    this.ed2 = fp("#endDate2",{
-      dateFormat:'d-m-Y',
-      //enableTime: true,
-      //time_24hr:true
-    });
-  }
-
-  sd1Open(x,y,z){
-    console.log("startData1 open...",x,y,z)
-  }
-
-  sd1Changed(selDates,strDate,instance){
-    console.log("startDate1 changed...", strDate);
-  }  
-
-  sd1Closed(selDates,strDate,instance){
-    console.log("startDate1 closed...", strDate);
-    //set enddate min value
-    debugger
-    this.setEd1MinDate(selDates[0]) 
   }
   /**
-   * Set EndDate 1 minDate value of datepicker
+   * Set minDate value of ed1 datepicker based on selected value in sd1 flatpickr
    * @param minDate 
    */
-  setEd1MinDate(minDate){
-    if (this.ed1){
-      this.ed1.set("minDate",minDate);
+  ed0MinDate(minDate){
+    if (this.ed[0]){
+      this.ed[0].set("minDate",minDate);
     }    
   }
-  ed1Open(x,y,z){
-    console.log("endDate open...", z)
+  ed0Open(x,y,z){
+    console.log("ed0 open...", z)
     //z.set("minDate",this.sd1.)
   }
+//-------------------------------------------------
+// SAMPLE 2 - DATE ONLY AND LOCALIZATION
+  initSample2(){
+    this.initSd1();
+    this.initEd1();
+  }
+  initSd1(){
+    //debugger
+    this.sd[1] = fp("#sd1",{
+      dateFormat:'d-m-Y',
+      locale: Dutch 
+      //enableTime: true,
+      //time_24hr:true
+    });
+  }
 
+  initEd1(){
+    //debugger
+    this.ed[1] = fp("#ed1",{
+      dateFormat:'d-m-Y',
+      locale: Russian
+      //enableTime: true,
+      //time_24hr:true
+    });
+  }
 
+//-------------------------------------------------
+// SAMPLE 3 - ONE DATE and TWO TIME INPUTS ONLY
+  initSample3(){
+    this.initSd2();
+    this.initSt0();
+    this.initEt0();
+  }
+  initSd2(){
+    //debugger
+    this.sd[2] = fp("#sd2",{
+      dateFormat:'d-m-Y',
+      locale: Dutch,
+      allowInput:true,
+      onClose: this.minTimeSt0.bind(this)
+      //enableTime: true,
+      //time_24hr:true
+    });
+  }
+  minTimeSt0(x,y,z){
+    if (this.st[0]){
+      //debugger 
+      //this.st[0].set("minDate",x[0]);
+      this.st[0].set("defaultHour",0);
+    }
+  }
+  initSt0(){
+    //debugger
+    this.st[0] = fp("#st0",{
+      enableTime: true,
+      time_24hr:true,
+      noCalendar: true,
+      dateFormat:'H:i',
+      locale: Dutch,
+      allowInput:true,
+      onClose: this.minTimeEt0.bind(this)
+    });
+  }
+  minTimeEt0(x,y,z){
+    if (this.et[0]){
+      this.et[0].set("minDate",x[0]);
+    }
+    /* not optimal
+    //debugger
+    let el = document.getElementById("et0");
+    el.value = x[0];
+    console.log(el);
+    */
+  }
+  initEt0(){
+    //debugger
+    this.et[0] = fp("#et0",{
+      enableTime: true,
+      time_24hr:true,
+      noCalendar: true,
+      dateFormat:'H:i',
+      locale: Dutch,
+      allowInput:true
+    });
+  }
 
 }
